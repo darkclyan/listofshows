b280ae9dd83578175fa20522a6d83886f4b0d1c5
darkclyan <gustavober59@gmail.com>
2020-02-22 18:52:27 -0300

    Add files via upload

--- default.php
+++ default.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="style.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
- 
+
+<div class="form-control"> 
 <form action="./listofshows.php" method="post">
-  <label for="name">Informe a Data:</label>
+  <label for="name" class="text-black">Informe a Data:</label>
   <input type="date" id="isdate" name="isdate">
   <input type="submit" value="Submit">
 </form>  
+</div>
 
 </body>
 </html>
--- listofshows.php
+++ listofshows.php
@@ -1,22 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="style.css">
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
  
-<div id="listShows"></div>
+<div id="listShows" class="text-black"></div>
  
 <script>
-var x = <?php echo date("Y-m-d", strtotime($_POST["isdate"])); ?>;
+
+var dt = "<?php echo $_POST['isdate']; ?>";
+
+var myObject = {country: "US", date: dt};
 
 $(document).ready(function() {
 function dadosJson() {
-    $.getJSON("http://api.tvmaze.com/schedule?country=US&date=2020-01-01", function(json) {
-        $.each(json, function() {
-            let info = '<p>' + "(" + this['airstamp'] + ") " + this['name'] + ": " + this['name'] + '</p>';
-            $('#listShows').append(info);
-        });
+    $.getJSON("http://api.tvmaze.com/schedule", myObject, function(json) {
+
+/*agrupar*/
+var items = {},
+base, key;
+$.each(json, function(index, val) {
+  key = val.show.network.name;
+  if (!items[key]) {
+    items[key] = [];
+  }
+  items[key].push("(" + val.airdate + " - " + val.airtime + ")" + " " + val.show.name + ": " + val.name );
+});
+
+/*montar*/
+var $select = $('#listShows');
+$.each(items, function(index, val) {
+
+var optgroup = $('<optgroup>');
+optgroup.attr('label', index);
+  
+$.each(val, function(index, val) {
+    optgroup.append($('<option>', {
+        text: val
+    }));
+});
+  
+$select.append(optgroup);
+});
     });
 }
     dadosJson();
